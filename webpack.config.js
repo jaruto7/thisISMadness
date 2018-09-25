a3ac3408f67866e2202cf4e13bfd8db36c482cc8
Jarek Kajko <jaruto7@gmail.com>
2018-09-25 19:18:48 +0200

    Create files from the beginning

--- webpack.config.js
+++ webpack.config.js
@@ -12,18 +12,6 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            module: true
-                        }
-                    }
-                ]
             }
         ]
     }
