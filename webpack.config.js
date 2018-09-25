1e33e1fc8e45ef9867870a75f69b245c27f5489c
Jarek Kajko <jaruto7@gmail.com>
2018-09-26 01:11:14 +0200

    Setup files to compile scripts into one file in build dir

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: {
-                            module: true
+                            modules: true
                         }
                     }
                 ]
