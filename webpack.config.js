a644fa4699d9c256696e8de78e0a15926aa295f5
Jarek Kajko <jaruto7@gmail.com>
2018-09-25 20:14:42 +0200

    Create files to start work with app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require( 'path' );
 
 // webpack.config.js
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve( __dirname, 'build' ),
         filename: 'app.bundle.js'
